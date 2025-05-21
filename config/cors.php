1f9e4871becf4d739a0e05447b47ab6f11cf6b6a
Danilo-Mosca <danilo.mosca85@gmail.com>
2025-05-21 20:58:31 +0200

    fixed cors.php file to allow access to all users/domains

--- config/cors.php
+++ config/cors.php
@@ -4,13 +4,14 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['*'],     // Accesso alle API consentito a tutti
 
-    'allowed_origins' => [env('APP_FRONTEND_URL', 'http://127.0.0.1:8000')], // il primo valore fa riferimento al contenuto della variabile d'ambiente nel file ".env"
+    // 'allowed_origins' => ['http://localhost:5174'], OPPURE USANDO LE VARIABILI D'AMBIENTE:
+    //'allowed_origins' => [env('APP_FRONTEND_URL', 'http://localhost:5174')],          // il primo valore fa riferimento al contenuto della variabile d'ambiente nel file ".env"
     // il secondo valore invece è un valore di default che sarà utilizzato nel caso in cui la chiave richiesta non è stata trovata nel file .env
     // Avrei potuto scrivere anche: 'allowed_origins' => ['*'], per consentire l'accesso a tutti i domini
     // oppure 'allowed_origins' => ['https://tuo-dominio.com'], per consentire l'accesso al solo dominio https://tuo-dominio.com
-    
+
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
